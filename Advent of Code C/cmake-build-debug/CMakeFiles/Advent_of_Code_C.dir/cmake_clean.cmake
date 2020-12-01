d28f405ff4c70113ff0973ef788d24a2f849253c
OliverKillane <oliverkillane@gmail.com>
2020-12-01 22:10:56 +0000

    a
    changes

---
+++ Advent of Code C/cmake-build-debug/CMakeFiles/Advent_of_Code_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Advent_of_Code_C.exe"
+  "Advent_of_Code_C.exe.manifest"
+  "Advent_of_Code_C.lib"
+  "Advent_of_Code_C.pdb"
+  "CMakeFiles/Advent_of_Code_C.dir/main.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Advent_of_Code_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
